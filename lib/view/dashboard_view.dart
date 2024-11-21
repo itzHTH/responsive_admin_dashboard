f20a1d3aed22d4aa099d12670e74c1c30e34d968
Leader <jhuth981@gmail.com>
2024-11-22 00:15:47 +0300

    add desktop layout

--- lib/view/dashboard_view.dart
+++ lib/view/dashboard_view.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_admin_dashboard/widgets/adaptive_layout_builder.dart';
+import 'package:responsive_admin_dashboard/widgets/desktop_layout.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return AdaptiveLayoutBuilder(
+        mobileLayout: (context) => const SizedBox(),
+        tabletLayout: (context) => const SizedBox(),
+        desktopLayout: (context) => const DesktopLayout());
   }
 }
